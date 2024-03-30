bde346ef3c784b94520494db4c13068b3c42686d
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-03-30 03:14:00 -0400

    added codes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 //-----using morgan/middleware and static files--
 app.use(morgan('dev')); 
 app.use(express.static('styles'));
+app.use(express.urlencoded({ extended: true }));
 
 // ----connecting to mongoDB
 const dbURI = 'mongodb+srv://pritee_perscholas:AGC6f1lik5pmwpaz@atlascluster.d9evfbt.mongodb.net/SBA308A';
@@ -53,6 +54,29 @@ app.get('/blogs' ,(req, res) => {
         console.log(err);
     });
 }) 
+
+//----POST REQUEST-----
+app.post('/blogs', (req, res) => {
+const blog = new Blog(req.body);
+
+blog.save()
+.then((result) => {
+    res.redirect('/blogs');
+}) .catch((err) => {
+    console.log(err);
+});
+})
+
+app.get('/blogs/:id', (req, res) => {
+const id = req.params.id;
+// console.log(id);
+Blog.findById(id)
+.then((result) => {
+    res.render('details', { blog: result, title: 'Blog Details'});
+}) .catch((err) => {
+    console.log(err);
+});
+})
 
 app.get('/blogs/create', (req, res) => {
     // res.send('<p>about Page</p>');
